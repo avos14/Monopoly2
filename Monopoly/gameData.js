c0df412034bda4bb3bd5f27abb701bc5fa004783
avos14 <adamvos1@gmail.com>
2023-05-01 15:48:19 +0300

    Add project files.

---
+++ Monopoly/gameData.js
@@ -0,0 +1,302 @@
+﻿game = {
+    steam: {
+        properties: [
+            {
+                group: "Corner",
+                name: "Start here<br/>Get 100$<br/> <--",
+                value: 0,
+                img: "null",
+            },
+            {
+                group: "Open World",
+                name: "Cyberpunk 2077",
+                value: 100,
+                img: "img/cp2077.jpg",
+
+            },
+            {
+                group: "Open World",
+                name: "The Witcher 3",
+                value: 50,
+                img: "img/witcher3.jpg",
+            },
+            {
+                group: "Open World",
+                name: "Skyrim",
+                value: 100,
+                img: "img/skyrim.jpg",
+            },
+            {
+                group: "Open World",
+                name: "GTA V",
+                value: 150,
+                img:"img/gtaV.jpg",
+            },
+            {
+                group: "Open World",
+                name: "Red Dead Redemption II",
+                value: 250,
+                img: "img/rdr2.jpg",
+            },
+            {
+                group: "Corner",
+                name: "Go to prison",
+                value: 0,
+                img: "null",
+            },
+            {
+                group: "First Person Shooting",
+                name: "Modern Warfare",
+                value: 100,
+                img: "img/codMW.jpg",
+            },
+            {
+                group: "First Person Shooting",
+                name: "Rainbow 6 Siege",
+                value: 100,
+                img: "img/r6s.jpg",
+            },
+            {
+                group: "First Person Shooting",
+                name: "Battlefield V",
+                value: 150,
+                img: "img/bf5.jpg",
+            },
+            {
+                group: "First Person Shooting",
+                name: "Counter Strike",
+                value: 50,
+                img: "img/cs.jpg",
+            },
+            {
+                group: "First Person Shooting",
+                name: "Team Fortress 2",
+                value: 150,
+                img: "img/tf2.jpg",
+            },
+            {
+                group: "Corner",
+                name: "Vacation",
+                value: 0,
+                img: "null",
+            },
+            {
+                group: "Battle Royale",
+                name: "Apex Legends",
+                value: 200,
+                img: "img/apex.jpg",
+            },
+            {
+                group: "Battle Royale",
+                name: "Fortnite",
+                value: 50,
+                img: "img/fortnite.jpg",
+            },
+            {
+                group: "Battle Royale",
+                name: "Spellbreak",
+                value: 100,
+                img: "img/spellbreak.jpg",
+            },
+            {
+                group: "Battle Royale",
+                name: "Warzone",
+                value: 50,
+                img: "img/warzone.jpg",
+            },
+            {
+                group: "Battle Royale",
+                name: "Worms Rumble",
+                value: 300,
+                img: "img/worms.jpg",
+            },
+            {
+                group: "Corner",
+                name: "Prison",
+                value: 0,
+                img: "null",
+            },
+            {
+                group: "Horror",
+                name: "Amnesia - Rebirth",
+                value: 150,
+                img: "img/amnesia.jpg",
+            },
+            {
+                group: "Horror",
+                name: "Alien Isolation",
+                value: 100,
+                img: "img/alien.jpg",
+            },
+            {
+                group: "Horror",
+                name: "Deadspace",
+                value: 100,
+                img: "img/deadspace.jpg",
+            },
+            {
+                group: "Horror",
+                name: "Outlast",
+                value: 250,
+                img: "img/outlast.jpg",
+            },
+            {
+                group: "Horror",
+                name: "Dying Light",
+                value: 150,
+                img:"img/dyinglight.jpg",
+            },
+
+        ]
+    },
+    lego: {
+        properties: [
+            {
+                group: "Corner",
+                name: "Start here<br/>Get 100$<br/> <--",
+                value: 0,
+                img: "null",
+            },
+            {
+                group: "Cars",
+                name: "Bugatti Chiron",
+                value: 100,
+                img: "img/bugatti.jpg",
+
+            },
+            {
+                group: "Cars",
+                name: "Ferrari F40",
+                value: 50,
+                img: "img/f40.jpg",
+            },
+            {
+                group: "Cars",
+                name: "Porsche GT3RS",
+                value: 100,
+                img: "img/gt3rs.jpg",
+            },
+            {
+                group: "Cars",
+                name: "Mustang 1967",
+                value: 150,
+                img: "img/mustang.jpg",
+            },
+            {
+                group: "Cars",
+                name: "Lamborghini Sian",
+                value: 250,
+                img: "img/lambo.jpg",
+            },
+            {
+                group: "Corner",
+                name: "Go to prison",
+                value: 0,
+                img: "null",
+            },
+            {
+                group: "Brickheadz",
+                name: "Thanos",
+                value: 100,
+                img: "img/thanos.jpg",
+            },
+            {
+                group: "Brickheadz",
+                name: "Iron Man",
+                value: 100,
+                img: "img/ironman.jpg",
+            },
+            {
+                group: "Brickheadz",
+                name: "Gamora",
+                value: 150,
+                img: "img/gamora.jpg",
+            },
+            {
+                group: "Brickheadz",
+                name: "Starlord",
+                value: 50,
+                img: "img/starlord.jpg",
+            },
+            {
+                group: "Brickheadz",
+                name: "Black Widow",
+                value: 150,
+                img: "img/widow.jpg",
+            },
+            {
+                group: "Corner",
+                name: "Vacation",
+                value: 0,
+                img: "null",
+            },
+            {
+                group: "Lego Technic",
+                name: "Bucket Excavator",
+                value: 200,
+                img: "img/bucket.jpg",
+            },
+            {
+                group: "Lego Technic",
+                name: "Hauler",
+                value: 50,
+                img: "img/hauler.jpg",
+            },
+            {
+                group: "Lego Technic",
+                name: "Liebherr R",
+                value: 100,
+                img: "img/escavator.jpg",
+            },
+            {
+                group: "Lego Technic",
+                name: "Crane",
+                value: 50,
+                img: "img/crane.jpg",
+            },
+            {
+                group: "Lego Technic",
+                name: "Transporter",
+                value: 300,
+                img: "img/transporter.jpg",
+            },
+            {
+                group: "Corner",
+                name: "Prison",
+                value: 0,
+                img: "null",
+            },
+            {
+                group: "Starwars",
+                name: "AT-AT",
+                value: 150,
+                img: "img/atat.jpg",
+            },
+            {
+                group: "Starwars",
+                name: "X-Wing",
+                value: 100,
+                img: "img/xwing.jpg",
+            },
+            {
+                group: "Starwars",
+                name: "Falcon",
+                value: 100,
+                img: "img/falcon.jpg",
+            },
+            {
+                group: "Starwars",
+                name: "Deathstar",
+                value: 250,
+                img: "img/deathstar.jpg",
+            },
+            {
+                group: "Starwars",
+                name: "Ren's Shuttle",
+                value: 150,
+                img: "img/shuttle.jpg",
+            },
+
+        ]
+    }
+}
